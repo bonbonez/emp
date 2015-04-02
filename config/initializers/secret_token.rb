80f72f5038a865f9edda9750593f9cd43a6100cf
Artem Petrov <bonbonez@gmail.com>
2015-04-02 10:19:20 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Empressa::Application.config.secret_key_base = 'f8af0630d0e3b98ea68f4092f83b40e3f93c52f0cbe283a72d09e185cbc059a7de14b2f059e9ff21a8ccc3bb632405843db82a2f9d3d78f2a96e7dcc440215a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Empressa::Application.config.session_store :cookie_store, key: '_empressa_session'
